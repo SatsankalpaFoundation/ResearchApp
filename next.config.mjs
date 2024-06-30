9545fa394c5321befe3a64d477282b24b4228016
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2024-06-30 08:09:47 +0530

    Fixed Stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-import TerserPlugin from 'terser-webpack-plugin';
-
-export function webpack(config, { isServer }) {
-    if (!isServer) {
-        config.optimization.minimizer = [
-            new TerserPlugin({
-                terserOptions: {
-                    compress: {
-                        drop_console: true, // Remove console.* statements
-                    },
-                },
-            }),
-        ];
-    }
-
-    return config;
-}
+export const experimental = {
+    missingSuspenseWithCSRBailout: false,
+};
